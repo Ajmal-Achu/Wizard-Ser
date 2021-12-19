85d0256d36aadd772b5cff08609486f7852f0cd8
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2021-12-20 00:14:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['918590967139','919744196187'] // Letakan nomor kamu disini
+global.owner = ['918590967139','919744196187','917012074386'] // Letakan nomor kamu disini
 global.mods = [] // Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
