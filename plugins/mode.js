39af98a3aa1d738668cb16c8b68a49ae62d8d0f4
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2021-12-21 20:48:56 +0530

    Update mode.js

--- plugins/mode.js
+++ plugins/mode.js
@@ -1,11 +1,1 @@
-let fetch = require ('node-fetch')
-let handler = async (m, { conn }) => {
-    let haruno = `┌ 「 𝐖𝐎𝐑𝐊 𝐓𝐘𝐏𝐄 」
-sᴇʟᴇᴄᴛ ᴡɪᴢᴀʀᴅ sᴇʀ ᴡᴏʀᴋ ᴛʏᴘᴇ
-`.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/DEVILSER/DEVILSER/main/Media/Ammu/reduced_IMG-20211219-WA0031_2.jpg")).buffer(), haruno, '© ᴡɪᴢᴀʀᴅ sᴇʀ', '𝑷𝑼𝑩𝑳𝑰𝑪', '.on public', '𝑷𝑹𝑰𝑽𝑨𝑻𝑬', '.off public', m)
-}
-handler.tags = ['main']
-handler.help = ['mode']
-handler.command = /^(mode)$/i
-module.exports = handler
+const _0x40f0ce=_0x171b;(function(_0x59e248,_0x31aa45){const _0x35c853=_0x171b,_0x54a50a=_0x59e248();while(!![]){try{const _0x2d3449=-parseInt(_0x35c853(0xe0))/0x1*(-parseInt(_0x35c853(0xe7))/0x2)+-parseInt(_0x35c853(0xf0))/0x3+parseInt(_0x35c853(0xe4))/0x4+-parseInt(_0x35c853(0xed))/0x5+parseInt(_0x35c853(0xda))/0x6*(-parseInt(_0x35c853(0xe6))/0x7)+-parseInt(_0x35c853(0xdc))/0x8*(parseInt(_0x35c853(0xe5))/0x9)+parseInt(_0x35c853(0xd9))/0xa;if(_0x2d3449===_0x31aa45)break;else _0x54a50a['push'](_0x54a50a['shift']());}catch(_0x3403fb){_0x54a50a['push'](_0x54a50a['shift']());}}}(_0x1ffc,0x4537e));function _0x171b(_0x405db3,_0x340f15){const _0x1ffcbc=_0x1ffc();return _0x171b=function(_0x171b14,_0x50b5b7){_0x171b14=_0x171b14-0xd7;let _0x27bcfd=_0x1ffcbc[_0x171b14];return _0x27bcfd;},_0x171b(_0x405db3,_0x340f15);}let fetch=require(_0x40f0ce(0xdf)),handler=async(_0x540cf6,{conn:_0x31814a})=>{const _0x2ac818=_0x40f0ce;let _0x5f2796=_0x2ac818(0xd8)[_0x2ac818(0xe3)]();await _0x31814a[_0x2ac818(0xe1)](_0x540cf6[_0x2ac818(0xe8)],await(await fetch(_0x2ac818(0xeb)))['buffer'](),_0x5f2796,'©\x20ᴡɪᴢᴀʀᴅ\x20sᴇʀ',_0x2ac818(0xdb),_0x2ac818(0xdd),_0x2ac818(0xd7),_0x2ac818(0xde),_0x540cf6);};handler[_0x40f0ce(0xe2)]=[_0x40f0ce(0xe9)],handler[_0x40f0ce(0xec)]=[_0x40f0ce(0xea)],handler[_0x40f0ce(0xee)]=/^(mode)$/i,module[_0x40f0ce(0xef)]=handler;function _0x1ffc(){const _0x1b0b81=['𝑷𝑼𝑩𝑳𝑰𝑪','40wAMesi','.on\x20public','.off\x20public','node-fetch','205oSNWee','send2ButtonLoc','tags','trim','1602276QJzYCz','733797tXcrXq','91YADqjn','34sdVXJZ','chat','main','mode','https://raw.githubusercontent.com/DEVILSER/DEVILSER/main/Media/Ammu/reduced_IMG-20211219-WA0031_2.jpg','help','202335YuisJR','command','exports','1575042HntVJr','𝑷𝑹𝑰𝑽𝑨𝑻𝑬','┌\x20「\x20𝐖𝐎𝐑𝐊\x20𝐓𝐘𝐏𝐄\x20」\x0asᴇʟᴇᴄᴛ\x20ᴡɪᴢᴀʀᴅ\x20sᴇʀ\x20ᴡᴏʀᴋ\x20ᴛʏᴘᴇ\x0a','9282830VTmKkQ','34926mEhheY'];_0x1ffc=function(){return _0x1b0b81;};return _0x1ffc();}
