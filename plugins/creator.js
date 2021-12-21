ff20388555bfb3279e5650846e654b39a6e3d7e8
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2021-12-21 16:19:25 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,10 @@
+/*
+   Coded by amal-dx
+*/
 function handler(m) {
-  this.sendContact(m.chat, '918590967139','919744196187'[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  this.sendContact(m.chat, '918590967139','919744196187' 'ᴀᴊᴍᴀʟ ᴀɴᴅ ᴀᴄʜᴜ', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
